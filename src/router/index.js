96a337e7caf26d1a998a2fe6a303469d263e23d8
Santi Jimenez <santiagojimenezisc@gmail.com>
2021-10-30 18:12:42 -0500

    Fix #1 busqueda

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,22 @@ const routes = [
   },
 ];
 
+router.beforeEach((to, from, next) => {
+  var subdir = window.location.host.split('.')[0];
+  var domain = 'santi.app';
+  var pageToLoad = 'Home';
+
+  if (subdir !== damin && to.name !== pageToLoad) {
+    next({
+      name: pageToLoad, params: {
+
+      }
+    })
+  } else {
+    next()
+  }
+})
+
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
